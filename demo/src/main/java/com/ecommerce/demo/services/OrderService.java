db4c552a50f00327005584bcda8c0b1351e0c8ac
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-06-01 14:27:02 +0530

    RazorPay Payment Gateway Integration

--- demo/src/main/java/com/ecommerce/demo/services/OrderService.java
+++ demo/src/main/java/com/ecommerce/demo/services/OrderService.java
@@ -2,16 +2,15 @@ package com.ecommerce.demo.services;
 
 import com.ecommerce.demo.models.Cart;
 import com.ecommerce.demo.models.Order;
-import com.ecommerce.demo.models.User;
 import com.ecommerce.demo.repositories.CartRepo;
 import com.ecommerce.demo.repositories.OrderRepo;
 import com.ecommerce.demo.repositories.UserRepo;
+import com.razorpay.RazorpayException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class OrderService {
@@ -25,6 +24,9 @@ public class OrderService {
     @Autowired
     UserRepo userRepo;
 
+    @Autowired
+    PaymentService paymentService;
+
     public List<Order> getAllOrders(){
         return orderRepo.findAll();
     }
@@ -33,12 +35,9 @@ public class OrderService {
         return orderRepo.findById(orderId).orElseThrow(()-> new RuntimeException("Order not Found"));
     }
 
-    public Order saveOrder(String cartId){
+    public Order saveOrder(String cartId) throws RazorpayException {
         Order order = new Order();
-        order.setId(UUID.randomUUID().toString());
-        order.setOrderCreation(new Date());
         order.setStatus("Pending");
-
         Cart cart = cartRepo.findById(cartId).orElseThrow(()-> new RuntimeException("Cart not Found"));
         order.setTotal(cart.getPrice());
         order.setProductList(cart.getProducts());
@@ -46,7 +45,12 @@ public class OrderService {
         cart.setProducts(null);
         cart.setQty(0);
         cart.setPrice(0);
-
+        com.razorpay.Order rz_Order=paymentService.createOrder(order.getTotal(), cartId);
+        if(rz_Order!=null){
+            order.setId(rz_Order.get("id"));
+            order.setStatus(rz_Order.get("status"));
+            order.setOrderCreation(rz_Order.get("created_at"));
+        }
         return orderRepo.save(order);
     }
 
